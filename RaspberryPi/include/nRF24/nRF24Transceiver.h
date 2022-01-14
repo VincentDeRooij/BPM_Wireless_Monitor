58372f32e17da932e528505aaa13d2e422a939eb
Vincent de Rooij <vincent.wjap.de.rooij@outlook.com>
2022-01-14 19:30:45 +0100

    Update

--- RaspberryPi/include/nRF24/nRF24Transceiver.h
+++ RaspberryPi/include/nRF24/nRF24Transceiver.h
@@ -9,7 +9,7 @@
 // >https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
 // >https://create.arduino.cc/projecthub/muhammad-aqib/nrf24l01-interfacing-with-arduino-wireless-communication-0c13d4
 
-#define CE_PIN 22   // GPIO pin on the Raspberry Pi
+#define CE_PIN 22 // GPIO pin on the Raspberry Pi
 #define CSN_PIN 8 // CE GPIO pin on the Raspberry Pi
 #define PAYLOAD_SIZE sizeof(int)
 #define PA_LEVEL RF24_PA_LOW
@@ -21,8 +21,8 @@ enum TRANCSEIVER_ROLE_TYPE
 };
 
 bool setupTransceiver();
-void setToTransmitterType(int payload); // Master type
-void setToReceiverType(int &payload);	// Slave type
+void setToTransmitterType(float payload); // Master type
+void setToReceiverType(int &payload);	  // Slave type
 //void setRoleOfTransceiver(TRANCSEIVER_ROLE_TYPE type);
 
 #endif // __NRF24TRANSCEIVER_H__
